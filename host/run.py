a9b3e5bdaf924aa33c20ad00190f95d7f7b44244
Tyler Leite <tyler.leite@yandex.com>
2016-03-01 17:27:38 -0500

    finished code for picking a game from command line args

--- host/run.py
+++ host/run.py
@@ -4,16 +4,24 @@ import sys
 from server import *
 
 if __name__ == "__main__":
+	# Get what game we're using from command line
+	#  might change this to use the config file
 	if not len(sys.argv) == 2:
+		# Improper usage, terminate
 		print "Usage:\n%s [game_module]" % argv[0]
 		sys.exit(1)
 	else:
+		# Never used importlib before, this might not work... needs testing
+		mod_name = "GGS.host.games.%s" % argv[1]
 		try:
-			RAIL = importlib.import_module(argv[1], __NAME__)
-		catch:
-			print "Error importing module: %s" % (__NAME__ + argv[1],)
+			# Import the library as RAIL
+			RAIL = importlib.import_module(mod_name)
+		except:
+			# Module doesn't exist, terminate
+			print "Error importing module: %s" % (mod_name)
 			sys.exit(1)
 
+	# Attempt to initiate the server with the dynamically imported game
 	server = Server(RAIL.game)
 	server.init()
 	server.go()
