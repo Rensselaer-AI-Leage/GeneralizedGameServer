cbe7de03db901e6f63d1ada588eae7396e00dfe7
edgchess <edgchess@aol.com>
2016-03-09 17:44:58 -0500

    fixed indents

--- host/run.py
+++ host/run.py
@@ -6,33 +6,33 @@ from server import *
 from helpers import config as cfg
 
 if __name__ == "__main__":
-        # Get what game we're using from command line
-        #  might change this to use the config file
-        if not len(sys.argv) == 2:
-                # Improper usage, terminate
-                print "Usage:\n%s [game_module]" % sys.argv[0]
-                sys.exit(1)
-        else:
-                # Never used importlib before, this might not work... needs testing
-                mod_name = "host.games.%s" % sys.argv[1]
-                try:
-                        # Import the library as RAIL
-                        RAIL = importlib.import_module(mod_name)
-                except:
-                        # Module doesn't exist, terminate
-                        print "Error importing module: %s" % (mod_name)
-                        sys.exit(1)
+	# Get what game we're using from command line
+	#  might change this to use the config file
+	if not len(sys.argv) == 2:
+		# Improper usage, terminate
+		print "Usage:\n%s [game_module]" % sys.argv[0]
+		sys.exit(1)
+	else:
+		# Never used importlib before, this might not work... needs testing
+		mod_name = "host.games.%s" % sys.argv[1]
+		try:
+			# Import the library as RAIL
+			RAIL = importlib.import_module(mod_name)
+		except:
+			# Module doesn't exist, terminate
+			print "Error importing module: %s" % (mod_name)
+			sys.exit(1)
 
-        # Get the proper config file based on game
-        cfg_file = os.path.join('..', 'host', 'games', '%s.cfg' % sys.argv[1])
-        settings = cfg.load(cfg_file)
+	# Get the proper config file based on game
+	cfg_file = os.path.join('..', 'host', 'games', '%s.cfg' % sys.argv[1])
+	settings = cfg.load(cfg_file)
 
-        # Could not load config file, probably it doesn't exist
-        if not settings:
-                print "Could not open configuration file. Terminating."
-                sys.exit(1)
+	# Could not load config file, probably it doesn't exist
+	if not settings:
+		print "Could not open configuration file. Terminating."
+		sys.exit(1)
 
-        # Attempt to initiate the server with the dynamically imported game
-        server = Server(RAIL.game, settings)
-        server.init()
-        server.go()
+	# Attempt to initiate the server with the dynamically imported game
+	server = Server(RAIL.game, settings)
+	server.init()
+	server.go()
