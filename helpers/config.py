a2bfc3302017a2fe3190916d1a6e489b5b1c6e96
Tyler Leite <tyler.leite@yandex.com>
2016-03-10 16:57:50 -0500

    Updated config parsing

--- helpers/config.py
+++ helpers/config.py
@@ -8,8 +8,13 @@ def load(filen):
 	try:
 		with open(filen) as cfg:
 			for line in cfg:
+				line = line.split('#')[0] # Comments, yay!
+				line = line.split('//')[0] # //Comments, yay!
 				parts = line.split(':')
-				config[parts[0]] = parts[1]
+				if len(parts) == 2:
+					config[parts[0].strip()] = parts[1].strip()
+				else:
+					pass # This is pythonic
 			return config
 	except Exception as e:
 		print "Error opening file " + filen
